5c5a1bc6ca92ec44ba59d31a9270f48251e79f9f
Amit <123800811+Amit3234@users.noreply.github.com>
2023-08-24 11:36:09 +0000

    conversion day3

---
+++ 3day_conversion.js
@@ -0,0 +1,45 @@
+// example 1
+// let  ex1 = '33'
+// console.log(typeof ex1)
+// console.log(ex1);
+// console.log(typeof Number(ex1));
+
+
+// example 2
+// let ex2 = "33abc"
+// console.log(ex2);
+// console.log(typeof ex2)
+// console.log(typeof Number(ex2));
+// console.log(ex2);
+
+
+// example 3
+// let ex3 = undefined
+// console.log(ex3);
+// console.log(typeof ex3)
+// console.log(typeof Number(ex3));
+// console.log(ex3);
+
+
+// example 4
+// let ex4 = 'game'
+// console.log(ex4);
+// console.log(typeof ex4)
+// console.log(typeof Number(ex4));
+// console.log(ex4);
+
+
+// example 5
+// let ex5 = true
+// console.log(ex5);
+// console.log(typeof ex5)
+// console.log(typeof String(ex5));
+// console.log(ex5);
+
+
+// example 6
+// let ex5 = true
+// console.log(ex5);
+// console.log(typeof ex5)
+// console.log(typeof String(ex5));
+// console.log(ex5);
