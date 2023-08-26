8af5a50dad44f35e6dc75fcf673f8f4fc95dfbbf
Amit <123800811+Amit3234@users.noreply.github.com>
2023-08-26 17:38:05 +0000

    comparision day4 add strict mode

--- 4day_conversion_operation.js
+++ 4day_conversion_operation.js
@@ -37,7 +37,8 @@
 
 
 
-comparision
+//  ==> comparision
+
 // console.log(2 > 1);
 // console.log(2 >= 1);
 // console.log(2 < 1);
@@ -57,5 +58,5 @@ console.log(undefined > 0);
 console.log(undefined < 0);
 
 // === 
-
+// strict mode 
 console.log("2" === 2);
