68358d57621c2b5703fc277b5034b88a1fa2bf7f
Amit <123800811+Amit3234@users.noreply.github.com>
2023-08-26 10:20:18 +0000

    conversion operation day4

---
+++ 4day_conversion_operation.js
@@ -0,0 +1,36 @@
+// ex 1
+// let ex = 3;
+// let example = -ex;
+// console.log(example);
+
+
+// ex 2 
+// console.log('1' + 2);
+// console.log(1 + '2');
+// console.log('1' + 2 + 2);
+// console.log(1 + 2 + '2');
+
+
+// ex 3
+//  console.log(+true)
+//  console.log(+'')
+//  show error
+//  console.log(true+) 
+
+
+// ex 4 
+// Pre-increment ++x
+// let x = 3;
+// let y = ++x;
+// console.log(x)
+// console.log(y)
+// console.log(y)
+
+
+// ex 5
+// Post-increment
+// let x = 3;
+// console.log(x)
+// let y = x++;
+// console.log(x)
+// console.log(y)
