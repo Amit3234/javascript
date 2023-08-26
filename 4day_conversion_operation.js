ee0d4976738aa0f1e50b4a2acea42218522f24a7
Amit <123800811+Amit3234@users.noreply.github.com>
2023-08-26 17:34:31 +0000

    comparision day4

--- 4day_conversion_operation.js
+++ 4day_conversion_operation.js
@@ -34,3 +34,28 @@
 // let y = x++;
 // console.log(x)
 // console.log(y)
+
+
+
+comparision
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
