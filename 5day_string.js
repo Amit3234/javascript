774ad441075e1ec237aae495f9736cd0464d7445
Amit <123800811+Amit3234@users.noreply.github.com>
2023-09-21 08:33:47 +0000

    5day_string

---
+++ 5day_string.js
@@ -0,0 +1,26 @@
+// String
+
+// example 1
+// let name = 'Ajay';
+
+// console.log(`my name is ${name}`)
+
+// example 2
+
+let game = new String('Color       red looks brighter than color red blue')
+// console.log(game.__proto__)
+// console.log(game.indexOf('s'))
+// console.log(game.lastIndexOf('a'))
+// console.log(game.search('red'))
+
+
+// example 3
+let str = "      The quick      ";
+// console.log(str)
+// console.log(str.trim())
+
+
+// example 4 
+let col = "Color red looks brighter than color blue.";
+// console.log(col.replace('red' , 'pink'))
+console.log((col.slice(2, 9)))
