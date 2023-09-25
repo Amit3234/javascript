c8e8a99f8b24393fe49a9059cdec272c4f0084f1
Amit <123800811+Amit3234@users.noreply.github.com>
2023-09-25 18:01:31 +0000

    6day math

---
+++ 6day_math.js
@@ -0,0 +1,102 @@
+// Math example 
+
+// 1 ceil 
+// 2 floor
+// 3 round
+// 4 trunc
+// 5 max 
+// 6 min
+// 7 sqrt 
+// 8 cbrt 
+// 9 pow 
+// 10 random 
+// 11 abs 
+// 12 pi 
+
+
+
+// example 1 
+// 1 ceil  
+// 33 = 33   
+let ex1 = Math.ceil(33.1)
+// console.log(ex1)
+
+
+// example 2
+// 2 floor
+// 33 = 33 
+const ex2 = Math.floor(33.9)
+// console.log(ex2)
+
+
+// example 3 
+// 3 round  => round figure
+// 3.1 = 3 
+// 3.5 = 4
+// 3.9 = 4
+const ex3 = Math.round(3.9)
+// console.log(ex3)
+
+
+// example 4 
+// 4 trunc
+// 3.1 = 3 
+const ex4 = Math.round(3.9)
+// console.log(ex4)
+
+
+// example 5
+// 5 max
+const ex5 = Math.max(3, 33, 6, 99, 20)
+// console.log(ex5)
+
+
+// example 6
+// 6 min
+const ex6 = Math.min(3, 33, 6, 99, 20)
+// console.log(ex6)
+
+
+// example 7
+// 7 sqrt
+// 4 = 2*2
+// 16 = 4*4
+const ex7 = Math.sqrt(25)
+// console.log(ex7)
+
+
+// example 8
+// 8 cbrt
+// 8 = 2*2*2
+// 64 = 4*44
+const ex8 = Math.cbrt(729)
+// console.log(ex8)
+
+
+// example 9
+// 9 pow
+// Math.pow(3, 2) ==> 3*3
+const ex9 = Math.pow(4, 3)
+// console.log(ex9)
+
+
+// example 10
+// 10 random
+// Math.trunc((Math.random() * 10)) 
+const ex10 = Math.random()
+// console.log(ex10)
+
+
+// example 11
+// 11 abs  ==> Absolute value
+// 2+4 = 6
+// 'hello' = NaN
+// -9.45 = 9.45
+const ex11 = Math.abs(4.45)
+// console.log(ex11)
+
+
+// example 12
+// 12 PI
+const ex12 = Math.PI
+// console.log(ex12)
