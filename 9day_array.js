6d0a0a23e5fc5320e2d7ecdcd74a9ba0a60190e8
Amit <123800811+Amit3234@users.noreply.github.com>
2023-09-26 19:22:42 +0000

    array example 2

--- 9day_array.js
+++ 9day_array.js
@@ -1,8 +1,17 @@
 // Array example 
 
 // example 1 
-let fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+let ex1 = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+// console.log(ex1[3])
+// console.log(ex1.length)
 
 
-console.log(fruits[3])
-console.log(fruits.length)
+// example 2 
+// push() ==> add end
+// unshift() ==> add beginning
+let ex2 = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+// ex2.push('game')
+// console.log(ex2)
+ex2.unshift('sum')
+console.log(ex2)
+
