c2d033f0708ab936a7d192b964a4c58eb12dd080
Amit <123800811+Amit3234@users.noreply.github.com>
2023-09-27 18:22:56 +0000

    using symbol in object

--- 10day_objects.js
+++ 10day_objects.js
@@ -4,6 +4,7 @@
 // Object.create 
 
 // object literals
+const sym = Symbol('game')
 const user = {
     name : "Ajay",
     age : 23,
@@ -11,12 +12,17 @@ const user = {
     location : "delhi",
     email : "ajay@google.com",
     login : true,
-    logday : ['mon', 'wed']
+    logday : ['mon', 'wed'],
+    [sym] : 'sum'
 }
 
-console.log(user.name);
-console.log(user['email']);
-console.log(user["full name"]);
+// console.log(user.name);
+// console.log(user['email']);
+// console.log(user["full name"]);
+// console.log(user.sym);
+// console.log(typeof user.sym);
+console.log(user[sym]);
+console.log(typeof user[sym]);
 
 
 
