7a165980ba2005017e256309962a9ba345640d2e
Amit <123800811+Amit3234@users.noreply.github.com>
2023-09-27 18:14:34 +0000

    object first example

---
+++ 10day_objects.js
@@ -0,0 +1,23 @@
+// objects example 
+
+// singleton ==> constructor 
+// Object.create 
+
+// object literals
+const user = {
+    name : "Ajay",
+    age : 23,
+    'full name' : "Ajay sharma",
+    location : "delhi",
+    email : "ajay@google.com",
+    login : true,
+    logday : ['mon', 'wed']
+}
+
+console.log(user.name);
+console.log(user['email']);
+console.log(user["full name"]);
+
+
+
+
