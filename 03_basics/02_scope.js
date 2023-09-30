7db85d1291b5126344fa70ac1e76f459e208446d
Amit <123800811+Amit3234@users.noreply.github.com>
2023-09-30 06:01:43 +0000

    add block scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,21 @@
+// JavaScript Scope 
+
+// JavaScript has 3 types of scope:
+
+// 1) Block scope
+// 2) Function scope
+// 3) Global scope
+
+// example 1 
+// 1) Block scope 
+// let and const
+
+{
+    const ex1 = 'game'
+    let exx1 = 'game1'
+    // console.log(ex1);
+    // console.log(exx1);
+}
+// error 
+// console.log(ex1);
+// console.log(exx1);
