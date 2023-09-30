965b02c172f2f179a992ae86abff0cce70297571
Amit <123800811+Amit3234@users.noreply.github.com>
2023-09-30 08:47:39 +0000

    this concept

---
+++ 03_basics/03_arrowfun.js
@@ -0,0 +1,21 @@
+// This and Arrow Function 
+
+// const user = {
+//     username : 'ajay',
+//     courseprice : 1000,
+
+//     welcomemessage : function (){
+//         console.log(`${this.username}, welcome to website`)
+//         // console.log(this)
+//     }
+// }
+
+// user.welcomemessage()
+// user.username = 'sam'
+// user.welcomemessage()
+// console.log(this)
+
+
+
+
+
