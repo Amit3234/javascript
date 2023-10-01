61972085679d2a590020b434ae42682a7fbeb97e
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-01 11:25:53 +0000

    iife function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// IIFE (Immediately Invoked Function Expression) 
+
+// example 1 
+(function ex1(){
+    console.log('game');
+})();
+
+
+// example 2 
+( (name) => {
+    console.log(`${name}`);
+})('ajay')
