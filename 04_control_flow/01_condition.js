8017cb83791a7ed5e6fc74cfb6fa2d7d62ab38dd
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-04 18:05:15 +0000

    explain condition

---
+++ 04_control_flow/01_condition.js
@@ -0,0 +1,13 @@
+// if else condition 
+
+// example 1
+const temp = 20;
+if (temp === 40){
+    console.log('grater than 20')
+}
+else if( temp === 20){
+    console.log('temp is 20')
+}
+else{
+    console.log('less than is 20')
+}
