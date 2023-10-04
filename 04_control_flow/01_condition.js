ded1a8e93c942e63cd2c047a7fc1bec0c7c226a2
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-04 18:44:59 +0000

    complete if else condition

--- 04_control_flow/01_condition.js
+++ 04_control_flow/01_condition.js
@@ -1,13 +1,37 @@
 // if else condition 
 
 // example 1
-const temp = 20;
-if (temp === 40){
-    console.log('grater than 20')
+// const temp = 20;
+// if (temp === 40){
+//     console.log('grater than 20')
+// }
+// else if( temp === 20){
+//     console.log('temp is 20')
+// }
+// else{
+//     console.log('less than is 20')
+// }
+
+
+// example 2
+// const game = 10
+// if (game > 5) {
+//     let power = 'win';
+//     console.log(`i am ${power} game`);
+// };
+// console.log(`i am ${power} game`);
+
+
+// example 3 
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
 }
-else if( temp === 20){
-    console.log('temp is 20')
-}
-else{
-    console.log('less than is 20')
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
 }
