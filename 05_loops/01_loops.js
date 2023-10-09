d002b6db66e9f1fc0a26eda3990faa490d538113
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-09 18:25:08 +0000

    complete for loop

--- 05_loops/01_loops.js
+++ 05_loops/01_loops.js
@@ -8,9 +8,28 @@
 
 
 // example 2
-for (let i = 0; i < 10; i++){
-    console.log(`outer loop ${i}`)
-    for (let j = 0; j < 10; j++) {
-        console.log(`start inner loop ${j}`)
+// for (let i = 0; i < 10; i++){
+//     console.log(`outer loop ${i}`)
+//     for (let j = 0; j < 10; j++) {
+//         // console.log(`start inner loop ${j}`)
+//         console.log(` ${i} * ${j} = ${i*j}`)
+//     }
+// }
+
+
+// example 3 
+// const myArray = ['game', 'batman', 'superman']
+// for( let i = 0; i<myArray.length; i++){
+//     let a = myArray[i]
+//     console.log(a)
+// }
+
+// example 4
+for (let index = 0; index < 10; index++) {
+    if (index === 5) {
+        console.log('detectd 5')
+        // break
+        // continue
     }
+    console.log(index)    
 }
