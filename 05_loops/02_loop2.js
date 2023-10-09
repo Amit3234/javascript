543c89fffa78c2f080d0ee5cf17f57f16d88a5be
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-09 18:39:52 +0000

    while loop complete

---
+++ 05_loops/02_loop2.js
@@ -0,0 +1,18 @@
+// Loops 
+// while and do while 
+
+
+// exmaple 1 
+// let ex1 = 0 
+// while(ex1 < 10){
+//     console.log(ex1)
+//     ex1++
+// }
+
+// example 2
+const myArray = ['game', 'batman', 'superman']
+let ex2 = 0 
+while(ex2 < myArray.length){
+    console.log(myArray[ex2])
+    ex2++
+}
