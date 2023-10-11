78c20977201f2db923acf180d90e4fdb6eb9a50f
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-11 09:15:00 +0000

    complete for each loop with example

--- 05_loops/05_loop5.js
+++ 05_loops/05_loop5.js
@@ -11,8 +11,30 @@ let ex1 = ["a", "b", "c", "d", "e", "f"];
 //     console.log(item)
 // })
 
-function game(item) {
-    console.log(item)
-}
+// function game(item,index, arr) {
+//     console.log(item, index, arr)
+// }
 
-ex1.forEach(game)
+// ex1.forEach(game)
+
+
+// example 2  
+const ex2 = [
+    {
+        lang : 'javascript',
+        short : 'js'
+    },
+    {
+        lang : 'java',
+        short : 'jav'
+    },
+    {
+        lang : 'python',
+        short : 'py'
+    },
+]
+
+ex2.forEach( (item, index, arr) => {
+    // console.log(item, index, arr)
+    console.log(item.lang)
+})
