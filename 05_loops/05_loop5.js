50a40f8e81007202cc9cc38cd07f421d59504730
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-11 09:01:32 +0000

    for each loop with 3 example

---
+++ 05_loops/05_loop5.js
@@ -0,0 +1,18 @@
+// foe each 
+
+
+let ex1 = ["a", "b", "c", "d", "e", "f"];
+
+// ex1.forEach( function (item) {
+//     // console.log(item)
+// })
+
+// ex1.forEach( (item) => {
+//     console.log(item)
+// })
+
+function game(item) {
+    console.log(item)
+}
+
+ex1.forEach(game)
