b5d68298423d447a0cdeaac39dbea07562df2f3d
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-11 08:49:12 +0000

    for in loop example

---
+++ 05_loops/04_loop4.js
@@ -0,0 +1,15 @@
+// for in loop 
+
+// example 1  
+const ex1 = {
+    game1 : 'NFS',
+    game2 : 'spiderman'
+}
+
+for (const key in ex1){
+    console.log(ex1[key])
+}
+
+
+
+// map not iterable using for in loop 
