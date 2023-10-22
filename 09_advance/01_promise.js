9f8a2b3db70f64b40aa964e7ff15a413f5882208
Amit <123800811+Amit3234@users.noreply.github.com>
2023-10-22 19:14:28 +0000

    promsie short trick example

--- 09_advance/01_promise.js
+++ 09_advance/01_promise.js
@@ -1,13 +1,24 @@
 // promise 
 
 // example 1
-const promiseOne = new Promise( function (resolve, reject) {
-    setTimeout(function () {
-        console.log('first promise')
+// const promiseOne = new Promise( function (resolve, reject) {
+//     setTimeout(function () {
+//         console.log('first promise')
+//         resolve()
+//     }, 1000)
+// })
+
+// promiseOne.then(function(){
+//     console.log('successful promise')
+// })
+
+
+// example 2
+new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        console.log('shortcut promise')
         resolve()
     }, 1000)
-})
-
-promiseOne.then(function(){
-    console.log('successful promise')
+}).then(function(){
+    console.log('run shortcut promise')
 })
