cc0c425de075f9995f94cd7bb1218d8185bf0577
Amit <123800811+Amit3234@users.noreply.github.com>
2023-11-07 19:26:03 +0000

    stack basic example

---
+++ 01_basics/08_stack_heap.js
@@ -0,0 +1,13 @@
+// Stack and heap memory
+
+// stack ==> stack is used to store static data example primitive data types
+// heap  ==> heap is used to store dynamic data example non-primitive data types
+ 
+
+// stack 
+// example
+let youName = 'Ajay'
+let myName = youName
+myName = 'Aman'
+console.log(youName)
+console.log(myName)
