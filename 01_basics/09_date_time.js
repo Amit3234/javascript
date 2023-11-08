b7552c2c6fe2470e59dfa9ef5c8c0519ce49b147
Amit <123800811+Amit3234@users.noreply.github.com>
2023-11-08 07:24:13 +0000

    add new file date and time

---
+++ 01_basics/09_date_time.js
@@ -0,0 +1,12 @@
+// date exmaple
+
+// exmaple 1
+let ex1 = new Date()
+// console.log(ex1)
+// console.log(ex1.toString())
+// console.log(ex1.toDateString())
+// console.log(ex1.toISOString())
+// console.log(ex1.toJSON())
+// console.log(ex1.toLocaleString())
+// console.log(ex1.toLocaleDateString())
+
