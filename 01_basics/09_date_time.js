6fbfda683444c18176c4800b69a094098d53aa01
Amit <123800811+Amit3234@users.noreply.github.com>
2023-11-08 07:46:36 +0000

    date and time concept complete with example

--- 01_basics/09_date_time.js
+++ 01_basics/09_date_time.js
@@ -1,5 +1,7 @@
 // date exmaple
 
+// date ++++++++++++++++++++
+
 // exmaple 1
 let ex1 = new Date()
 // console.log(ex1)
@@ -14,5 +16,16 @@ let ex1 = new Date()
 // exmaple 2  
 const ex2 = new Date(2023, 0, 22)
 const ex22 = new Date('12-01-2023')
-console.log(ex2.toLocaleString())
-console.log(ex22.toLocaleString())
+// console.log(ex2.toLocaleString())
+// console.log(ex22.toLocaleString())
+
+
+// time +++++++++++++++++++
+
+// exmaple 3 
+let ex3 = new Date()
+// console.log(ex3.getTime())
+// console.log(Math.floor(Date.now()/1000))
+// console.log(ex3.getMonth() + 1)
+// console.log(ex3.getDate())
+// console.log(ex3.getFullYear())
