9aecdac313c2a4ed7c198db1f6dd340550dcbe4e
Amit <123800811+Amit3234@users.noreply.github.com>
2023-11-10 08:50:08 +0000

    call stack exmaple

--- 02_basics/05_call_stack.js
+++ 02_basics/05_call_stack.js
@@ -1,5 +1,7 @@
 // important exmaple + video
 
+
+// exmaple 1
 let a = 10;
 let b = 5;
 function addnum(num1, num2){
@@ -10,3 +12,18 @@ let result1 = addnum(a, b)
 let result2 = addnum(2, 4)
 console.log(result1)
 console.log(result2)
+
+
+// exmaple 2
+function one(){
+  console.log('one')
+  two()
+}
+function two(){
+  console.log('two')
+  three()
+}
+function three(){
+  console.log('three')
+}
+one()
