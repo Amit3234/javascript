8cf22cb0beb203f232ef779aedd21e2f49a75ff6
Amit <123800811+Amit3234@users.noreply.github.com>
2023-11-10 08:43:49 +0000

    add call stack with exmaple

---
+++ 02_basics/05_call_stack.js
@@ -0,0 +1,12 @@
+// important exmaple + video
+
+let a = 10;
+let b = 5;
+function addnum(num1, num2){
+  let total = num1 + num2
+  return total
+}
+let result1 = addnum(a, b)
+let result2 = addnum(2, 4)
+console.log(result1)
+console.log(result2)
